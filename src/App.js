b1120c8f13fd4399c06de540602663f6451e700f
Ann Stone <ann@thestones.me>
2024-04-27 19:23:48 -0400

    Edit the beginning text

--- src/App.js
+++ src/App.js
@@ -24,33 +24,32 @@ function App() {
           </div>
         </div>
         <div className='text'>
-          Take a guess, how many questions does the introduction to programming Piazza course at the University of Michigan have every semester?
-        </div>
+          Take a guess - how many questions does the introduction to programming Piazza course at the University of Michigan receive every semester?        </div>
         <div className='text'></div>
         <ContinuousSlider />
         <div className='text'>
-          If it took staff members on average 5 minutes to answer a question, that’s 447 hours of staff time devoted towards answering questions each semester. What if we could reduce that time, though, by reducing the number of questions? This allows staff to reallocate time to teaching students how to debug their own code in office hours. It could lead to students being helped sooner in office hours, which would increase their chances of success.
-        </div>
+          If it took staff members on average 5 minutes to answer a question, that’s 447 hours of staff time devoted towards answering questions each semester. What if we could reduce that time, though? By reducing the number of questions, staff can reallocate time to teaching students one on one in office hours, which can directly increase student knowledge and success.        </div>
         <div className='text'>
           In order to reduce the number of questions, we first must find out what the questions are about. By finding commonly asked questions and struggle points, we can answer student questions before they have to ask them. Let’s begin our investigation by focusing on project 3 during the Winter 2023 semester. 
         </div>
         <div className='text'></div>
         <WordCloud />
         <div className='text'>
-          Here is a word cloud of 30 the most frequently used words in student questions. The biggest words that stand out are function, error, and test. They also happen to be the words used in the most unique posts. It’s no surprise that function is amongst the most frequently used words because the project itself asks students to code specific functions, so in order to describe a problem the word function must be used to give accurate context. What is more interesting, though, is why “error” and “test” are used so frequently. Are there common aspects about tests that confuse students? Let’s do a deeper dive into the actual questions to find out.
+          The word cloud above shows the most frequently used words in student questions. Evaluating the most frequently used words can give us an approximation of what general concepts spur the most questions. The biggest words that stand out are function, error, and test. They also happen to be the words used in the most unique posts. It’s no surprise that “function” is amongst the most frequently used words because project 3 asks students to code specific functions, so describing a problem requires the word “function”. What is more interesting, though, is why “error” and “test” are used so frequently. What aspects of these concepts do students find confusing? Let’s do a deeper dive into the actual questions to find out.
         </div>
         <NetworkDiagram width={550} height={550} data={data} id={'one'} />
         <div className='text'>
-          Here are some more words
+          Each dot in the network graph represents one question asked in project 3 Winter 2023 that contains the word “error” in it. Any dots connected by a line have a similarity of above 60%. There are 16 groups of similar posts that can reveal the most confusing concepts for students. Looking at the biggest group of similar questions positioned in the center of the graph, each question asks a version of “what does this error message mean”. This shows a potential gap in the teaching and resources provided by the course - reading and understanding error messages. By providing these resources, staff can spend less time explaining the parts of an error message and more time helping students fix these error messages.         
         </div>
-        <NetworkDiagram width={550} height={550} data={instructorData} id={'two'} />
         <div className='text'>
-          Finally some more words!
+          Error messages are an integral part of the Introduction to Programming course, thus are prevalent every semester. If students are confused on error messages in Winter 2023, are students in other semesters as well? Let’s find out.
         </div>
         <Boxplot width={550} height={550} data={boxplotData} />
         <div className='text'>
-          this is the last text before opening up to the dashboard
+          The graph above compares each question in the Winter 2023 semester to all questions in the previous semester and finds its max similarity in each semester. The distribution across semesters is fairly the same across semesters, with most posts falling in the 35% to 55% range. Questions that have this much similarity may be able to give students enough information to answer their own question. A quarter of the questions are nearly identical, falling in the 55% to 85% range. These are questions that instructors have to answer again and again each semester. Having questions this similar each semester reveals the amount of staff time wasted re answering questions. Providing more resources for confusing concepts will be beneficial for students each semester.        
         </div>
+        {/* <NetworkDiagram width={550} height={550} data={instructorData} id={'two'} /> */}
+
       </div>
     </div>
   );
